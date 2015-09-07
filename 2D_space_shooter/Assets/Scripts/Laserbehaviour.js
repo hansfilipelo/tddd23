77e5892292ba550de9480b9bcf9399285196130d
Johan Wikström <johwi672@student.liu.se>
2015-09-07 11:01:06 +0200

    Playercontrols typ done
    Kan navigera och skjuta laser

---
+++ 2D_space_shooter/Assets/Scripts/Laserbehaviour.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var rb: Rigidbody2D;
+var Speed : float;
+
+function Update () {
+
+rb.MovePosition(rb.position + Vector2(0,Speed) * Time.fixedDeltaTime);
+
+}
---
+++ 2D_space_shooter/Assets/Scripts/Player_controls.js
@@ -0,0 +1,48 @@
+﻿
+#pragma strict
+
+var MoveUp : KeyCode;
+var MoveDown : KeyCode;
+var MoveLeft : KeyCode;
+var MoveRight : KeyCode;
+var Shoot : KeyCode;
+var Speed : float;
+var shootCount : int=0;
+
+var rb : Rigidbody2D;
+
+var Upperboundry_x : float;
+var Upperboundry_y : float;
+var Lowerboundry_x : float;
+var Lowerboundry_y : float;
+
+var laser : Transform;
+
+function Update () {
+	if (Input.GetKey(MoveUp) && rb.position[1] < Upperboundry_y)
+	{
+	rb.MovePosition(rb.position + Vector2(0,Speed) * Time.fixedDeltaTime);
+	}
+	else if(Input.GetKey(MoveDown) && rb.position[1] > Lowerboundry_y)
+	{
+	rb.MovePosition(rb.position + Vector2(0,-Speed) * Time.fixedDeltaTime);
+	}
+	else if(Input.GetKey(MoveLeft) && rb.position[0] > Lowerboundry_x)
+	{
+	rb.MovePosition(rb.position + Vector2(-Speed,0) * Time.fixedDeltaTime);
+	}
+	else if(Input.GetKey(MoveRight) && rb.position[0] < Upperboundry_x)
+	{
+	rb.MovePosition(rb.position + Vector2(Speed,0) * Time.fixedDeltaTime);
+	}
+	
+	if(Input.GetKey(Shoot))
+	{
+		if (shootCount >= 10){
+				Instantiate(laser, rb.position +Vector2(0,1) , Quaternion.identity);
+				shootCount = 0;
+			}
+	}
+	
+	shootCount++;
+}
