7141a9a763242a6bb6ea40b50ab85d8263ac9e6b
hansfilipelo <hansfilipelo@gmail.com>
2015-09-07 11:26:05 +0200

    Fix memory leak in lasers.

--- 2D_space_shooter/Assets/Scripts/Laserbehaviour.js
+++ 2D_space_shooter/Assets/Scripts/Laserbehaviour.js
@@ -4,7 +4,10 @@ var rb: Rigidbody2D;
 var Speed : float;
 
 function Update () {
-
-rb.MovePosition(rb.position + Vector2(0,Speed) * Time.fixedDeltaTime);
-
+	
+	rb.MovePosition(rb.position + Vector2(0,Speed) * Time.fixedDeltaTime);
+	
+	if(rb.position[1] > 6){
+		Destroy(this.gameObject);
+	}
 }
