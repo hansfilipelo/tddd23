d94e6dcd00dbd0938b054bb7572d532c04f4f874
hansfilipelo <hansfilipelo@gmail.com>
2015-09-14 11:06:52 +0200

    Meteors now move with some form of gravitysimulation

--- 2D_space_shooter/Assets/Scripts/Meteorbehaviour.js
+++ 2D_space_shooter/Assets/Scripts/Meteorbehaviour.js
@@ -5,20 +5,27 @@ var speedMax : float;
 var speedMin : float;
 var Speed : float;
 var spread : float;
-var counter : float=0;
+var counter : float;
+var scaler : float;
 var xMovement : float;
+var counterMax : float;
 
 function Start() {
+	counter=0;
+	scaler=counterMax + 1;
 	Speed = Random.Range(speedMin,speedMax);
 	xMovement = Random.Range(-spread,spread);
 }
 
 function Update () {
 	
-	if (counter >= 500){
+	if (counter >= counterMax){
 		xMovement = Random.Range(-spread,spread);
+		counter = 1;
+		scaler = counterMax + 1;
 	}
-	rb.MovePosition(rb.position + Vector2(xMovement,-Speed) * Time.fixedDeltaTime);
+	rb.MovePosition(rb.position + Vector2(xMovement * Mathf.Min(scaler/counter,counter/scaler),-Speed) * Time.fixedDeltaTime);
 	counter++;
+	scaler--;
 }
 
