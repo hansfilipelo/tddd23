49b5d966eebb742dafc2a5201739cfd1a7dfb9f6
hansfilipelo <hansfilipelo@gmail.com>
2015-09-14 10:46:15 +0200

    Meteors now spawns and moves somewhat random

--- 2D_space_shooter/Assets/Scripts/Meteorbehaviour.js
+++ 2D_space_shooter/Assets/Scripts/Meteorbehaviour.js
@@ -1,11 +1,24 @@
 ﻿#pragma strict
 
 var rb: Rigidbody2D;
+var speedMax : float;
+var speedMin : float;
 var Speed : float;
+var spread : float;
+var counter : float=0;
+var xMovement : float;
+
+function Start() {
+	Speed = Random.Range(speedMin,speedMax);
+	xMovement = Random.Range(-spread,spread);
+}
 
 function Update () {
-
-rb.MovePosition(rb.position + Vector2(0,-Speed) * Time.fixedDeltaTime);
-
+	
+	if (counter >= 500){
+		xMovement = Random.Range(-spread,spread);
+	}
+	rb.MovePosition(rb.position + Vector2(xMovement,-Speed) * Time.fixedDeltaTime);
+	counter++;
 }
 
