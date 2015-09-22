d33092ad68348fd1f1d580c84ec925f521238ff1
hansfilipelo <hansfilipelo@gmail.com>
2015-09-22 15:53:13 +0200

    One shot meteor spawn

---
+++ 2D_space_shooter/Assets/Scripts/Level1Script.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var meteorFab : Transform;
+var meteorSpawn : float;
+var meteorSpawnTime : float;
+var meteorsSpawned : float;
+
+function Start () {
+	meteorsSpawned = 0;
+	meteorSpawnTime = Time.time + meteorSpawn;
+}
+
+function Update () {
+	if(!meteorsSpawned && Time.time > meteorSpawnTime){
+		meteorsSpawned = 1;
+		Instantiate(meteorFab, Vector2(Random.Range(Player_controls.Upperboundry_x, Player_controls.Lowerboundry_x),Player_controls.Upperboundry_y+1) , Quaternion.identity);
+		Instantiate(meteorFab, Vector2(Random.Range(Player_controls.Upperboundry_x, Player_controls.Lowerboundry_x),Player_controls.Upperboundry_y+1) , Quaternion.identity);
+		Instantiate(meteorFab, Vector2(Random.Range(Player_controls.Upperboundry_x, Player_controls.Lowerboundry_x),Player_controls.Upperboundry_y+1) , Quaternion.identity);
+		Instantiate(meteorFab, Vector2(Random.Range(Player_controls.Upperboundry_x, Player_controls.Lowerboundry_x),Player_controls.Upperboundry_y+1) , Quaternion.identity);
+	}
+}
