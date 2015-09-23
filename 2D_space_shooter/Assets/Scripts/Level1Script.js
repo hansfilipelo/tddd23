efc14cd5c1cb81427374fb4d1dc5bbe0b3ed451e
hansfilipelo <hansfilipelo@gmail.com>
2015-09-23 14:32:26 +0200

    Enemy2 is there but does not shoot yet

--- 2D_space_shooter/Assets/Scripts/Level1Script.js
+++ 2D_space_shooter/Assets/Scripts/Level1Script.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 
 var meteorFab : Transform;
-var meteorSpawn : float;
+var meteorWaveSpawn : float;
 var meteorSpawnTime : float;
 var meteorsSpawned : float;
 
@@ -10,18 +10,29 @@ var enemy1SpawnTime : float;
 var enemyWave1Time : float;
 var enemyWave1Spawned : float;
 
+var enemy2Fab : Transform;
+var enemy2SpawnTime : float;
+var enemyWave2Time : float;
+var enemyWave2Spawned : float;
+
+
 function Start () {
-	
+
 	// Set up scenes outer edges
 	Gizmos.color = Color.white;
 	//var width = 1;
 	var bottomLeft = Vector2(Player_controls.Lowerboundry_x, Player_controls.Lowerboundry_y);
 	var topLeft = Vector2(Player_controls.Lowerboundry_x, Player_controls.Upperboundry_y);
 	Debug.DrawLine(bottomLeft, topLeft);
-	
+
 	meteorsSpawned = 0;
-	meteorSpawnTime = Time.time + meteorSpawn;
+	meteorSpawnTime = Time.time + meteorWaveSpawn;
+
+	enemyWave1Spawned = 0;
 	enemy1SpawnTime = Time.time + enemyWave1Time;
+
+	enemyWave2Spawned = 0;
+	enemy2SpawnTime = Time.time + enemyWave2Time;
 }
 
 
@@ -44,16 +55,26 @@ function enemy1Wave(nr : int){
 
 // --
 
+function enemy2Wave(nr : int){
+	enemyWave2Spawned = 1;
+	for(var i = 0; i < nr; i++){
+		Instantiate(enemy2Fab, Vector2(Player_controls.Lowerboundry_x+i,Player_controls.Upperboundry_y+1), Quaternion.identity);
+	}
+}
 
 //--
 
 function Update () {
-	
+
 	if(!meteorsSpawned && Time.time > meteorSpawnTime){
 		meteorWave(4);
 	}
-	
+
 	if(!enemyWave1Spawned && Time.time > enemy1SpawnTime){
 		enemy1Wave(4);
+	}
+
+	if(!enemyWave2Spawned && Time.time > enemy2SpawnTime){
+		enemy2Wave(1);
 	}
 }
---
+++ 2D_space_shooter/Assets/Scripts/enemy2Behaviour.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var rb: Rigidbody2D;
+var xMovement : float;
+var speed : float;
+var stopAndHoverPos : float;
+var direction;
+
+function Start () {
+	direction = "right";
+	stopAndHoverPos = Player_controls.Upperboundry_y-1.5;
+}
+
+function Update () {
+
+
+	if(rb.position[1] < stopAndHoverPos && direction == "right"){
+		rb.MovePosition(rb.position+ Vector2(1,0) * Time.fixedDeltaTime * speed);
+		if(rb.position[0] > Player_controls.Upperboundry_x-0.5){
+			direction = "left";
+		}
+	}
+	else if(rb.position[1] < stopAndHoverPos && direction == "left"){
+		rb.MovePosition(rb.position+ Vector2(-1,0) * Time.fixedDeltaTime * speed);
+		if(rb.position[0] < Player_controls.Lowerboundry_x+0.5){
+			direction = "right";
+		}
+	}
+	else {
+		rb.MovePosition(rb.position + Vector2(0,-1) * Time.fixedDeltaTime * speed);
+	}
+
+	if(rb.position[1] < Player_controls.Lowerboundry_y){
+		Destroy(this.gameObject);
+	}
+}
