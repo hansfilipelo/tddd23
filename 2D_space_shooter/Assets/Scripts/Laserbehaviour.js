e36083ae3245987bd52c10f1c0ad5b93260962b7
Johan Wikström <johwi672@student.liu.se>
2015-09-23 20:33:43 +0200

    Musik och pangpang
    Musik laserljud och explosioner, allt i ett

--- 2D_space_shooter/Assets/Scripts/Laserbehaviour.js
+++ 2D_space_shooter/Assets/Scripts/Laserbehaviour.js
@@ -1,8 +1,9 @@
 ﻿#pragma strict
 
 var rb: Rigidbody2D;
+var hitSound : AudioClip;
 var Speed : float;
-
+var Explosion : Transform;
 function Update () {
 	
 	rb.MovePosition(rb.position + Vector2(0,Speed) * Time.fixedDeltaTime);
@@ -17,13 +18,20 @@ function OnCollisionEnter2D(collision : Collision2D) {
 
      	if (collision.gameObject.name=="Playbutton") {
      		
+     		
      		Application.LoadLevel ("mainScene");
    		}else if(collision.gameObject.name=="Leaderboard"){
+   	
+     		
      		
      		Application.LoadLevel ("Leaderboard");
      	}else if(collision.gameObject.name=="Backbutton"){
+     		
+     		
      		Application.LoadLevel ("Startmenu");
      	}else{
+     		AudioSource.PlayClipAtPoint(hitSound, rb.position);
+     		Instantiate(Explosion, rb.position, Quaternion.identity);
         	Destroy(collision.gameObject); // destroy it
        		Destroy(this.gameObject);
     }
